89222cca130df76f864b16b8ed1312aa44fa133b
Robert Collins <robertc@robertcollins.net>
2006-07-01 04:07:54 +1000

    Documentation.

--- __init__.py
+++ __init__.py
@@ -16,7 +16,28 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 # 
 
-"""The plugin entry point for loom."""
+"""Loom is a bzr plugin which adds new commands to manage a loom of patches.
+
+The commands are:
+ * loomify: This converts a branch into a loom enabled branch. As a result
+   of this, the branch format is converted and you need to have the loom
+   plugin installed to use it after that. The current branch nickname becomes
+   the base thread in the loom.
+
+ * create-thread: This adds a new thread to the loom with the supplied name
+   and positions the branch on the new thrad.
+
+ * show-loom: Shows the threads in the loom. It currently does not show the
+   # of commits in each thread, but it is planned to do that in the future.
+
+ * down-thread: Move the branch down a thread. After doing this commits and 
+   merges in this branch will affect the newly selected thread.
+
+ * up-thread: Move the branch up a thread. This will merge in all the changes
+   from the current thread that are not yet integrated into the new thread into
+   it and leave you ready to commit them.
+
+"""
 
 
 import bzrlib.commands
